3068bab3bc456e3c7b91efd44301bb95115a0f06
Marcos Acosta <marcos@acosta.tv>
2011-05-19 20:05:41 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails',     :git => 'git://github.com/rails/rails.git'
+
+gem 'pg'
+
+gem 'devise'
+
+gem 'slim'
+gem 'sass'
+gem 'coffee-script'
+
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'rspec-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeerRun::Application.config.secret_token = '20d005b6aa7e9595e5eb796ac81540e8d0127b21791d7860960b474096a10c6b277cba0b1d8cfabb67b89bddbcad1a732d9c57806c3392c1bbd447c5afc7c316'
