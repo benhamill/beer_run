578e5ce827668597e86988807108238cb65f08e0
Ben Hamill <git-commits@benhamill.com>
2011-06-09 03:51:33 -0500

    Code that describes points and how voting works. This should be in the DB, though, for speed.

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,2 +1,17 @@
 class Beer < ActiveRecord::Base
+  validates_uniqueness_of :name
+
+  def points
+    5 * first_place_votes +
+    4 * second_place_votes +
+    3 * third_place_votes +
+    2 * fourth_place_votes +
+    1 * fifth_place_votes
+  end
+
+  User::VOTE_NAMES.each do |name|
+    define_method("#{name}_place_votes") do
+      User.where("#{name}_place_beer_id" => self.id).count
+    end
+  end
 end
