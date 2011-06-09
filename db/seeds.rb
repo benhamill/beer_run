bdbed15d7a30dd48cc14ddf40b0a85a9b1042e9b
Ben Hamill <git-commits@benhamill.com>
2011-06-09 03:57:57 -0500

    Correct beer name.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ Beer.create :name => 'Independence Convict Hill'
 Beer.create :name => 'Natural Bohemian'
 Beer.create :name => 'Strongbow (Actually Cider)'
 Beer.create :name => 'Negra Modelo'
-Beer.create :name => "Fireman's Five"
+Beer.create :name => "Fireman's Four"
 Beer.create :name => 'Dos XX (lager)'
 Beer.create :name => 'Peroni'
 
