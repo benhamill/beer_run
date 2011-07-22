56cbf3d7f9db585d7aa9711bf2828756c698f1bc
Ben Hamill <git-commits@benhamill.com>
2011-07-22 14:37:32 -0500

    Make test fail without exceptions.

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -2,4 +2,8 @@ class BeersController < ApplicationController
   def index
     @beers = Beer.all.sort_by { |beer| -beer.points }
   end
+
+  def vote
+   @beers = Beer.order(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 BeerRun::Application.routes.draw do
   resources :beers, :only => [:index]
+  match '/vote' => 'beers#vote'
 
   devise_for :users
 
