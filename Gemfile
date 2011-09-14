03b725975274cdce8f1dda776480d29104d3124e
Ben Hamill <git-commits@benhamill.com>
2011-09-14 00:06:04 -0500

    Upgrade to Rails 3.1, I think.

--- Gemfile
+++ Gemfile
@@ -1,18 +1,26 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.1.0rc4'
+gem 'rails', '3.1.0'
 
 gem 'pg'
 gem 'thin'
 
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
 gem 'devise'
 gem 'slim'
-gem 'sass'
-gem 'coffee-script'
 gem 'jquery-rails'
-gem 'uglifier'
 
 group :development, :test do
+  gem 'pry'
   gem 'heroku'
   gem 'rspec-rails'
+  # Pretty printed test output
+  gem 'turn', :require => false
 end
--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,18 @@
 require File.expand_path('../boot', __FILE__)
 
-# Pick the frameworks you want:
+# require 'rails/all'
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
-# require "rails/test_unit/railtie"
+require "sprockets/railtie"
 
-# If you have a Gemfile, require the gems listed there, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  Bundler.require *Rails.groups(:assets => %w(development test))
+  # If you want your assets lazily compiled in production, use this line
+  # Bundler.require(:default, :assets, Rails.env)
+end
 
 module BeerRun
   class Application < Rails::Application
@@ -35,13 +38,6 @@ module BeerRun
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Please note that JavaScript expansions are *ignored altogether* if the asset
-    # pipeline is enabled (see config.assets.enabled below). Put your defaults in
-    # app/assets/javascripts/application.js in that case.
-    #
-    # JavaScript files you want as :defaults (application.js is always included).
-    # config.action_view.javascript_expansions[:defaults] = %w(prototype prototype_ujs)
-
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
@@ -50,5 +46,8 @@ module BeerRun
 
     # Enable the asset pipeline
     config.assets.enabled = true
+
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
   end
 end
