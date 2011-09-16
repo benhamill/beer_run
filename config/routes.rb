6849fc27ca31002f56560647aa2f9bdc5c33069c
Ben Hamill <git-commits@benhamill.com>
2011-09-15 23:45:30 -0500

    Don't need this.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 BeerRun::Application.routes.draw do
   resources :beers, :only => [:index, :new, :create]
-  # resources :passwords, :only => [:edit, :update]
   get '/vote' => 'beers#vote'
   post '/vote' => 'beers#record_vote'
 
