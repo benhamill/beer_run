450d3d353cc4a1b77c5a33a6abdcfdb07ae56fb1
Ben Hamill <git-commits@benhamill.com>
2012-02-04 00:00:09 -0600

    What? This is Ruby 1.9.2, bitches.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,17 @@
   model.delete_all
 end
 
-Beer.create :name => 'Independence Convict Hill'
-Beer.create :name => 'Natural Bohemian'
-Beer.create :name => 'Strongbow (Actually Cider)'
-Beer.create :name => 'Negra Modelo'
-Beer.create :name => "Fireman's Four"
-Beer.create :name => 'Dos XX (lager)'
-Beer.create :name => 'Peroni'
+Beer.create name: 'Independence Convict Hill'
+Beer.create name: 'Natural Bohemian'
+Beer.create name: 'Strongbow (Actually Cider)'
+Beer.create name: 'Negra Modelo'
+Beer.create name: "Fireman's Four"
+Beer.create name: 'Dos XX (lager)'
+Beer.create name: 'Peroni'
 
-User.create :email => 'bob@gmail.com', :password => 'password'
-User.create :email => 'jen@gmail.com', :password => 'password'
-User.create :email => 'ted@gmail.com', :password => 'password'
+User.create email: 'bob@gmail.com', password: 'password'
+User.create email: 'jen@gmail.com', password: 'password'
+User.create email: 'ted@gmail.com', password: 'password'
 
 User.find_each do |user|
   beers = Beer.order('random()').limit(5)
