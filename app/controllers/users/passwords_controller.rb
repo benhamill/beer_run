eaf63a100b4225343287c7185f39cadb48c3d291
Ben Hamill <git-commits@benhamill.com>
2012-03-10 04:14:11 -0600

    Just use the base devise for now.

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,4 +1,4 @@
 class Users::PasswordsController < Devise::PasswordsController
-  skip_before_filter :require_no_authentication, only: [:edit, :update]
-  before_filter :authenticate_user!, only: [:edit, :update]
+  # skip_before_filter :require_no_authentication, only: [:edit, :update]
+  # before_filter :authenticate_user!, only: [:edit, :update]
 end
