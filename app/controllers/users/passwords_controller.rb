740380260d72b32c996bcc34df1590280196180b
Ben Hamill <git-commits@benhamill.com>
2012-03-10 03:03:25 -0600

    Work around some weirdness in Devise.

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,20 +1,4 @@
 class Users::PasswordsController < Devise::PasswordsController
-  # before_filter :authenticate_user!
-
-  def edit
-    Rails.logger.info("\n\n\nWOAH!\n\n\n\n")
-    render text: 'WOAH' and return
-    @user = current_user
-  end
-
-  def update
-    @user = current_user
-
-    if @user.update_with_password(params[:user])
-      sign_in(@user, :bypass => true)
-      redirect_to root_url, :notice => "Password updated!"
-    else
-      render :edit
-    end
-  end
+  skip_before_filter :require_no_authentication, only: [:edit, :update]
+  before_filter :authenticate_user!, only: [:edit, :update]
 end
