b3dba59854e6721fdf4696d6a857b668ad7faf03
Ben Hamill <git-commits@benhamill.com>
2012-03-10 02:50:29 -0600

    Pretty up some links and urls.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ BeerRun::Application.routes.draw do
   get '/vote' => 'beers#vote'
   post '/vote' => 'beers#record_vote'
 
-  devise_for :users, controllers: { passwords: 'users/passwords' }
+  devise_for :users, controllers: { passwords: 'users/passwords' }, path_names: { sign_in: :log_in, sign_out: :log_out }
 
   root :to => 'beers#index'
 end
