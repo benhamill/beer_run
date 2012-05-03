e4285b089103a66e4d2dd60d639cb4aa751d86a6
Ben Hamill <git-commits@benhamill.com>
2012-05-02 23:12:37 -0500

    Hack in change password form.

--- app/controllers/users/passwords_controller.rb
+++
@@ -1,4 +0,0 @@
-class Users::PasswordsController < Devise::PasswordsController
-  # skip_before_filter :require_no_authentication, only: [:edit, :update]
-  # before_filter :authenticate_user!, only: [:edit, :update]
-end
